557b2c560af91d0ac2075a30ff1ca49ffaa2269e
sugusbby <37831357+sugusbby@users.noreply.github.com>
2018-03-27 15:43:49 +0530

    Create leap year or not.c

---
+++ beginner level/leap year or not.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void) {
+   int year;
+   year = 1993;
+   
+   if (((year % 4 == 0) && (year % 100!= 0)) || (year%400 == 0))
+      printf("%d is a leap year", year);
+   else
+      printf("%d is not a leap year", year);
+	return 0;
+}
